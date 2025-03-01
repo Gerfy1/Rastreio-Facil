48d6269da15f87e92721cfb9d0dbd8a180ccad7b
Gerfy1 <gerfynlol@gmail.com>
2025-03-01 17:02:24 -0300

    initialize project with Maven wrapper and Spring Boot setup

---
+++ src/main/java/com/rastreiofacil/rastreamento_correios/RastreamentoCorreiosApplication.java
@@ -0,0 +1,13 @@
+package com.rastreiofacil.rastreamento_correios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreamentoCorreiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastreamentoCorreiosApplication.class, args);
+	}
+
+}
