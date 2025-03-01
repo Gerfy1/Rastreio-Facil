3c1e5dd0a8637127b0ad77656fe4bc757e8d0461
Gerfy1 <gerfynlol@gmail.com>
2025-03-01 18:23:16 -0300

    update CorreiosService to include retry logic for tracking requests and modify User entity to use a reserved table name

--- src/main/java/com/rastreiofacil/rastreamento_correios/model/User.java
+++ src/main/java/com/rastreiofacil/rastreamento_correios/model/User.java
@@ -1,11 +1,9 @@
 package com.rastreiofacil.rastreamento_correios.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "\"user\"")
 public class User {
 
     @Id
--- src/main/java/com/rastreiofacil/rastreamento_correios/service/CorreiosService.java
+++ src/main/java/com/rastreiofacil/rastreamento_correios/service/CorreiosService.java
@@ -1,6 +1,7 @@
 package com.rastreiofacil.rastreamento_correios.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -8,13 +9,37 @@ public class CorreiosService {
 
     private final RestTemplate restTemplate;
 
+    private final String user = "teste";
+    private final String token = "1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f";
+
 
     public CorreiosService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public String getTrackingInfo(String trackingCode) {
-        String url = "https://api-correios.herokuapp.com/track/" + trackingCode;
-        return restTemplate.getForObject( url + trackingCode, String.class);
+        String url = "https://api.linketrack.com/track/json?user=" +user + "&token=" + token+ "&codigo="+trackingCode;
+        int maxRetries = 3;
+        int retryCount = 0;
+        int delay = 5000;
+
+        while (retryCount < maxRetries){
+            try {
+                return restTemplate.getForObject( url, String.class);
+            } catch (HttpClientErrorException.TooManyRequests e) {
+                retryCount++;
+                if (retryCount >= maxRetries){
+                    throw e;
+                }
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                    throw  new RuntimeException("Thread interrupted", ie);
+                }
+            }
+        }
+
+        throw new RuntimeException("Failed to get tracking info after " + maxRetries + " retries");
     }
 }
