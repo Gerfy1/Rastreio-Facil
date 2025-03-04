ef3650979aee0a91b3d3580f0088ce365e28f1bd
Gerfy <110788311+Gerfy1@users.noreply.github.com>
2025-03-04 00:44:44 -0300

    Update TrackingCodeController.java

--- src/main/java/com/rastreiofacil/rastreamento_correios/controller/TrackingCodeController.java
+++ src/main/java/com/rastreiofacil/rastreamento_correios/controller/TrackingCodeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/trackingCodes")
+@RequestMapping("/tracking-Codes")
 public class TrackingCodeController {
 
     @Autowired
